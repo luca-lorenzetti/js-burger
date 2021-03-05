0f03ddec74d0f33b5f9b02bc58a0404156e4fb9a
luca <luca.lorenzetti0@gmail.com>
2021-03-05 16:24:24 +0100

    Inserita parte Javascript

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// lista Coupons
+var coupons = ["viva10","dieta200","economico30"];
+
+// Salvo tutti gli elementi che mi servono
+var ingredients = document.getElementsByClassName("ingredient");
+var total = document.getElementById("total");
+var burgerName = document.getElementById("burgerName");
+var coupon = document.getElementById("coupon");
+
+// Aggiungo l'evento click al button
+var button = document.getElementById("calculate").addEventListener("click",function(){
+    var sum = 50;
+
+    if( !burgerName.value ){
+        alert("Devi inserire un nome!");
+    }
+    else{
+        for (var i = 0; i < ingredients.length; i++) {
+        
+            if(ingredients[i].checked) {
+                sum += parseInt(ingredients[i].value);
+            }
+        }
+
+        if( coupon.value ){
+            if(coupons.includes(coupon.value)){
+                coupons.splice(coupons.indexOf(coupon.value),1)
+                sum -= sum *0.2;
+                console.log("Coupons rimasti: " + coupons.length);
+                console.log(coupons);
+
+            }
+        }
+        total.innerText = sum.toFixed(2) + "€";
+    }
+});
+
